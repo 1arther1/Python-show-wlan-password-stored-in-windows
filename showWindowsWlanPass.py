e32fcc47bab4b9ac4117215b3618d160928a4d3f
1arther1 <arther.tirie@gmail.com>
2020-07-16 20:10:27 +0700

    wlan show pass

---
+++ showWindowsWlanPass.py
@@ -0,0 +1,28 @@
+import os
+def timer():
+    import time
+    t = 3
+    print ("akan berlangsung 3 detik")
+    while (t >= 0):
+        print (t, end='...')
+        time.sleep(1)
+        t -= 1
+        print ('')
+timer()
+lagi = True
+while(lagi == True):
+    os.system("netsh wlan show profiles")
+    a = input("masukkan nama ssid untuk lihat password : ")
+    os.system('netsh wlan show profiles name="{}" key=clear'.format(a))
+    
+    while True:
+        g = input("ingin lihat lagi (t/y) ?  ")
+        if (g == "t"):
+            lagi = False
+        elif (g == "y"):
+            break
+        else:
+            print("input salah")
+            
+        break
+            
